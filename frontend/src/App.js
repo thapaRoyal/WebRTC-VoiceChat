1f947204151f5a7893b2a612fa91c667cf09dcdc
thapaRoyal <thaparoyal17@gmail.com>
2022-01-13 19:50:41 +0545

    Implementing React router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
-  return <div className="App">This is our HomePage.</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
