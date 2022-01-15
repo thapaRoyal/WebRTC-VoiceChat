971ccec39e366476cf26a0225d57207d80ca0e89
thapaRoyal <thaparoyal17@gmail.com>
2022-01-15 16:40:02 +0545

    module and inline css

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Navigation.module.css';
 
 const Navigation = () => {
+  const brandStyle = {
+    color: '#fff',
+    textDecoration: 'none',
+    fontWeight: 'bold',
+    fontSize: '22px',
+    display: 'flex',
+    alignItems: 'center',
+  };
+
+  const logoText = {
+    marginLeft: '10px',
+  };
+
   return (
-    <nav className="container">
-      <Link to="/">
+    <nav className={`${styles.navbar} container`}>
+      <Link style={brandStyle} to="/">
         <img src="/images/logo.png" alt="coders house logo" />
-        <span>CodersHouse</span>
+        <span style={logoText}>Codershouse</span>
       </Link>
     </nav>
   );
