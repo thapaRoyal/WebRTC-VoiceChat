d5ce195c5458e6c413d6bcaff9094f658a1076c6
thapaRoyal <thaparoyal17@gmail.com>
2022-01-15 16:12:20 +0545

    global styles

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <nav>
+    <nav className="container">
       <Link to="/">
         <img src="/images/logo.png" alt="coders house logo" />
         <span>CodersHouse</span>
