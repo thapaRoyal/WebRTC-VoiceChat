ee76c2b53b6c1d43c11458068b668dd39e64fcbc
thapaRoyal <thaparoyal17@gmail.com>
2022-01-15 16:09:39 +0545

    logo

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
-  return <div>This is navigation.</div>;
+  return (
+    <nav>
+      <Link to="/">
+        <img src="/images/logo.png" alt="coders house logo" />
+        <span>CodersHouse</span>
+      </Link>
+    </nav>
+  );
 };
 
 export default Navigation;
