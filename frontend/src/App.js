443040a709f058ab2028986826d2998672836504
thapaRoyal <thaparoyal17@gmail.com>
2022-01-15 15:52:43 +0545

    implementing navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Navigation from './components/shared/Navigation/Navigation';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" exact element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navigation = () => {
+  return <div>This is navigation.</div>;
+};
+
+export default Navigation;
