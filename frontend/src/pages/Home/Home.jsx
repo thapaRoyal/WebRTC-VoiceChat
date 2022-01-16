d9d7ac38502061a3ea7f04c1f1f453a4059ee173
thapaRoyal <thaparoyal17@gmail.com>
2022-01-16 23:57:28 +0545

    creating home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import styles from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <div>Home Page!</div>;
+  return (
+    <div className={styles.card}>
+      <div className={styles.headingWrapper}>
+        <img src="/images/logo.png" alt="logo" />
+        <h1>Welcome to Codershouse</h1>
+      </div>
+      <p>
+        We’re working hard to get Codershouse ready for everyone! While we wrap
+        up the finishing youches, we’re adding people gradually to make sure
+        nothing breaks :)
+      </p>
+      <div>
+        <button>
+          <span>Get your username</span>
+          <img src="/images/arrow_forward.png" alt="arrow" />
+        </button>
+      </div>
+      <div>
+        <span>Have an invite text?</span>
+        <Link to="/login">Sign in</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
