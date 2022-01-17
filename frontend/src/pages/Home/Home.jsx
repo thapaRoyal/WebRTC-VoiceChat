20d55baeba0281bb909ee196f70d675f3c1c674a
thapaRoyal <thaparoyal17@gmail.com>
2022-01-17 20:34:01 +0545

    cardWrapper

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,25 +4,27 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className={styles.card}>
-      <div className={styles.headingWrapper}>
-        <img src="/images/logo.png" alt="logo" />
-        <h1 className={styles.heading}>Welcome to Codershouse</h1>
-      </div>
-      <p className={styles.text}>
-        We’re working hard to get Codershouse ready for everyone! While we wrap
-        up the finishing youches, we’re adding people gradually to make sure
-        nothing breaks :)
-      </p>
-      <div>
-        <button>
-          <span>Get your username</span>
-          <img src="/images/arrow_forward.png" alt="arrow" />
-        </button>
-      </div>
-      <div>
-        <span>Have an invite text?</span>
-        <Link to="/login">Sign in</Link>
+    <div className={styles.cardWrapper}>
+      <div className={styles.card}>
+        <div className={styles.headingWrapper}>
+          <img src="/images/logo.png" alt="logo" />
+          <h1 className={styles.heading}>Welcome to Codershouse</h1>
+        </div>
+        <p className={styles.text}>
+          We’re working hard to get Codershouse ready for everyone! While we
+          wrap up the finishing youches, we’re adding people gradually to make
+          sure nothing breaks :)
+        </p>
+        <div>
+          <button>
+            <span>Get your username</span>
+            <img src="/images/arrow_forward.png" alt="arrow" />
+          </button>
+        </div>
+        <div>
+          <span>Have an invite text?</span>
+          <Link to="/login">Sign in</Link>
+        </div>
       </div>
     </div>
   );
