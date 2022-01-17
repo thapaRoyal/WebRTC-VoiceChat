d9c3ad9760e1796c3fdd543d890a9218e272d886
thapaRoyal <thaparoyal17@gmail.com>
2022-01-17 20:31:44 +0545

    .text

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <img src="/images/logo.png" alt="logo" />
         <h1 className={styles.heading}>Welcome to Codershouse</h1>
       </div>
-      <p>
+      <p className={styles.text}>
         We’re working hard to get Codershouse ready for everyone! While we wrap
         up the finishing youches, we’re adding people gradually to make sure
         nothing breaks :)
