9573ced5fbe35832f216cf4e9fb1c1d954815489
thapaRoyal <thaparoyal17@gmail.com>
2022-01-21 21:15:55 +0545

    import Rgister

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Navigation from './components/shared/Navigation/Navigation';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
