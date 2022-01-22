a7a853109515b7d37c9ee92e242a36d2fd5cac34
thapaRoyal <thaparoyal17@gmail.com>
2022-01-22 16:44:57 +0545

    import

--- frontend/src/pages/Steps/StepOtp/StepOtp.jsx
+++ frontend/src/pages/Steps/StepOtp/StepOtp.jsx
@@ -1,7 +1,11 @@
+// Import
 import React from 'react';
+import styles from './StepOtp.module.css';
 
+// StepOtp Component
 const StepOtp = () => {
   return <div>StepOtp</div>;
 };
 
+// Export
 export default StepOtp;
