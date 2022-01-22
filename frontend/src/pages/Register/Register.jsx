1d691a8f8a138be31827511d66299c5f09aaea28
thapaRoyal <thaparoyal17@gmail.com>
2022-01-22 16:30:31 +0545

    steps

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import styles from './Register.module.css';
+
+// Registration steps
+const steps = {
+  1: StepPhoneEmail,
+  2: StepOtp,
+  3: StepName,
+  4: StepAvatar,
+  5: StepUsername,
+};
 
 const Register = () => {
-  return <div></div>;
+  return <div>This is register page</div>;
 };
 
 export default Register;
