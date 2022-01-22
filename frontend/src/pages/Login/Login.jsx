6c87b56e2939e295a0ddf5bc1e05faae0aefb540
thapaRoyal <thaparoyal17@gmail.com>
2022-01-22 18:11:49 +0545

    Login component and steps

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+// Import
+import React, { useState } from 'react';
+import StepOtp from '../Steps/StepOtp/StepOtp';
+import StepPhoneEmail from '../Steps/StepPhoneEmail/StepPhoneEmail';
 
-const Login = () => {
-  return <div>Login</div>;
+// Login steps
+const steps = {
+  1: StepPhoneEmail,
+  2: StepOtp,
 };
 
+//Login Component
+const Login = () => {
+  // local state
+  const [step, setStep] = useState(1);
+
+  // storing steps in a variable
+  const Step = steps[step];
+
+  // onNext function
+  function onNext() {
+    setStep(step + 1);
+  }
+  return <Step onNext={onNext} />;
+};
+
+// Export
 export default Login;
