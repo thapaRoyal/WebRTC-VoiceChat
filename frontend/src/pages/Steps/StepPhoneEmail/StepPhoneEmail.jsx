b69f555d5eaaa4b39a62d1b4b11f3d25ad59e82c
thapaRoyal <thaparoyal17@gmail.com>
2022-01-22 16:44:14 +0545

    import

--- frontend/src/pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
@@ -1,7 +1,11 @@
+// Import
 import React from 'react';
+import styles from './StepPhoneEmail.module.css';
 
+// StepPhoneEmail Component
 const StepPhoneEmail = () => {
   return <div>StepPhoneEmail</div>;
 };
 
+// Export
 export default StepPhoneEmail;
