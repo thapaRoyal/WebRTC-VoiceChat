b09798043df0a8651139c98d8085f70269e2affc
thapaRoyal <thaparoyal17@gmail.com>
2022-01-22 16:43:29 +0545

    import

--- frontend/src/pages/Steps/StepUsername/StepUsername.jsx
+++ frontend/src/pages/Steps/StepUsername/StepUsername.jsx
@@ -1,7 +1,11 @@
+// Import
 import React from 'react';
+import styles from './StepUsername.module.css';
 
+// StepUsername Component
 const StepUsername = () => {
   return <div>Step username</div>;
 };
 
+// Export
 export default StepUsername;
