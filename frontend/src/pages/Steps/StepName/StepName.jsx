6913676b0dc4ede798b2e55b1021cc561ae33f78
thapaRoyal <thaparoyal17@gmail.com>
2022-01-22 16:45:39 +0545

    import

--- frontend/src/pages/Steps/StepName/StepName.jsx
+++ frontend/src/pages/Steps/StepName/StepName.jsx
@@ -1,7 +1,11 @@
+// Import
 import React from 'react';
+import styles from './StepName.module.css';
 
+// StepName Component
 const StepName = () => {
   return <div>StepName</div>;
 };
 
+//  Export
 export default StepName;
