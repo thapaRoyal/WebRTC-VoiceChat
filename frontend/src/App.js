007580d1eed76073e08a5dde93a96d597129962e
thapaRoyal <thaparoyal17@gmail.com>
2022-01-22 18:08:13 +0545

    login componnt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Navigation from './components/shared/Navigation/Navigation';
 import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
 
 // App Component
 function App() {
