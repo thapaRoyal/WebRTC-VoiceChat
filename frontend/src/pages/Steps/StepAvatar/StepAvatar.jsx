080aff5ca36dacea427e2d80a4c9dbe7c49de51f
thapaRoyal <thaparoyal17@gmail.com>
2022-01-22 16:42:14 +0545

    import style

--- frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
+++ frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
@@ -1,7 +1,10 @@
+// Imports
 import React from 'react';
+import styles from './StepAvatar.module.css';
 
+// StepAvatar component
 const StepAvatar = () => {
   return <div>StepAvatar</div>;
 };
-
+// Export
 export default StepAvatar;
