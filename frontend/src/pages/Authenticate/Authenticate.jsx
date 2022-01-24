0933da5fb604a7d45aad72273218784f7ac0bf2a
thapaRoyal <thaparoyal17@gmail.com>
2022-01-24 19:05:55 +0545

    authenticate ..

--- frontend/src/pages/Authenticate/Authenticate.jsx
+++ frontend/src/pages/Authenticate/Authenticate.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+// Import
+import React, { useState } from 'react';
+import StepOtp from '../Steps/StepOtp/StepOtp';
+import StepPhoneEmail from '../Steps/StepPhoneEmail/StepPhoneEmail';
 
-const Authenticate = () => {
-  return <div></div>;
+// Login steps
+const steps = {
+  1: StepPhoneEmail,
+  2: StepOtp,
 };
 
+//Authenticate Component
+const Authenticate = () => {
+  // local state
+  const [step, setStep] = useState(1);
+
+  // storing steps in a variable
+  const Step = steps[step];
+
+  // onNext function
+  function onNext() {
+    setStep(step + 1);
+  }
+  return <Step onNext={onNext} />;
+};
+
+// Export
 export default Authenticate;
