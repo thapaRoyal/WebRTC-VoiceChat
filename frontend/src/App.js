2b8dc08afcebffc517374aec33eb2e12ee1c2e20
thapaRoyal <thaparoyal17@gmail.com>
2022-01-24 19:07:06 +0545

    authenticate route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home';
 import Navigation from './components/shared/Navigation/Navigation';
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login';
+import Authenticate from './pages/Authenticate/Authenticate';
 
 // App Component
 function App() {
@@ -16,11 +17,14 @@ function App() {
         <Route path="/" exact element={<Home />} />
       </Routes>
       <Routes>
+        <Route path="/authenticate" exact element={<Authenticate />} />
+      </Routes>
+      {/* <Routes>
         <Route path="/register" exact element={<Register />} />
       </Routes>
       <Routes>
         <Route path="/login" exact element={<Login />} />
-      </Routes>
+      </Routes> */}
     </BrowserRouter>
   );
 }
