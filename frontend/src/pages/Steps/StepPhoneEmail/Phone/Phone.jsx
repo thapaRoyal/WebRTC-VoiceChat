df4032a772464c848a42c0aaa449bfcb02a7c704
thapaRoyal <thaparoyal17@gmail.com>
2022-01-29 18:34:33 +0545

    creating phone component

--- frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Button from '../../../../components/shared/Button/Button';
+import Card from '../../../../components/shared/Card/Card';
 
 const Phone = () => {
-  return <div>Phone Component</div>;
+  return (
+    <Card title="Enter your phone number" icon="phone">
+      <div>
+        <Button text="Next" icon="arrow_forward" />
+      </div>
+    </Card>
+  );
 };
 
 export default Phone;
