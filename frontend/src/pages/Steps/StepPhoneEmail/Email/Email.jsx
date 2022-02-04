54ec8f291fc4c5b6c642454affde9f76b4ba9f96
thapaRoyal <thaparoyal17@gmail.com>
2022-02-04 18:19:11 +0545

    email component

--- frontend/src/pages/Steps/StepPhoneEmail/Email/Email.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/Email/Email.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Button from '../../../../components/shared/Button/Button';
+import Card from '../../../../components/shared/Card/Card';
 
 const Email = () => {
-  return <div>Email Component</div>;
+  return (
+    <Card title="Enter your email id" icon="email">
+      <div>
+        <Button text="Next" icon="arrow_forward" />
+      </div>
+    </Card>
+  );
 };
 
 export default Email;
