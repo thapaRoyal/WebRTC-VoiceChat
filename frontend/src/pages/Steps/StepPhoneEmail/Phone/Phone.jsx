db2bf213a75ea99556ddb8356c2d933f0f87bf8b
thapaRoyal <thaparoyal17@gmail.com>
2022-02-05 19:57:19 +0545

    input styles

--- frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Button from '../../../../components/shared/Button/Button';
 import Card from '../../../../components/shared/Card/Card';
 import TextInput from '../../../../components/shared/TextInput/TextInput';
+import styles from '../StepPhoneEmail.module.css';
 
 const Phone = () => {
   const { phoneNumber, setPhoneNumber } = useState('');
@@ -12,7 +13,9 @@ const Phone = () => {
         onChange={(e) => setPhoneNumber(e.target.value)}
       />
       <div>
-        <Button text="Next" icon="arrow_forward" />
+        <div className={styles.actionButtonWrap}>
+          <Button text="Next" icon="arrow_forward" />
+        </div>
       </div>
     </Card>
   );
