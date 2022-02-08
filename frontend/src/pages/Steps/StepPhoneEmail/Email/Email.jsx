0fe5fb263c6590195e537abc98ebb949b65da16c
thapaRoyal <thaparoyal17@gmail.com>
2022-02-08 19:51:49 +0545

    input

--- frontend/src/pages/Steps/StepPhoneEmail/Email/Email.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/Email/Email.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../../../components/shared/Button/Button';
 import Card from '../../../../components/shared/Card/Card';
+import TextInput from '../../../../components/shared/TextInput/TextInput';
+import styles from '../StepPhoneEmail.module.css';
 
 const Email = () => {
+  const { email, setEmail } = useState('');
+
   return (
     <Card title="Enter your email id" icon="email">
+      <TextInput value={email} onChange={(e) => setEmail(e.target.value)} />
       <div>
         <Button text="Next" icon="arrow_forward" />
       </div>
