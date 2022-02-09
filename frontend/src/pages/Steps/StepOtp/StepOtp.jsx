088d2a9ae3ab5de6e4da45f6b8b1abf1147b55b6
thapaRoyal <thaparoyal17@gmail.com>
2022-02-09 20:36:22 +0545

    stepOtp component

--- frontend/src/pages/Steps/StepOtp/StepOtp.jsx
+++ frontend/src/pages/Steps/StepOtp/StepOtp.jsx
@@ -1,13 +1,27 @@
 // Import
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../../../components/shared/Button/Button';
+import Card from '../../../components/shared/Card/Card';
+import TextInput from '../../../components/shared/TextInput/TextInput';
 import styles from './StepOtp.module.css';
 
 // StepOtp Component
 const StepOtp = ({ onNext }) => {
+  const [otp, setOtp] = useState('');
   return (
     <>
-      <div>StepOtp</div>
-      <button onClick={onNext}>Next</button>
+      <div className={styles.cardWrapper}>
+        <Card title="Enter the code we just texted you" icon="lock-emoji">
+          <TextInput value={otp} onChange={(e) => StepOtp(e.target.value)} />
+          <div className={styles.actionButtonWrap}>
+            <Button onClick={next} text="Next" />
+          </div>
+          <p className={styles.bottomParagraph}>
+            By entering your number, you're agreeing to our Terms of Service and
+            privacy policy. Thanks!
+          </p>
+        </Card>
+      </div>
     </>
   );
 };
