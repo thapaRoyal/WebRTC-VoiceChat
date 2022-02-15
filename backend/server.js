bf62a5c8d5221ea8f51b111bcfd1190c9b76df70
thapaRoyal <thaparoyal17@gmail.com>
2022-02-15 20:05:49 +0545

    creating server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
 require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5500;
+
+app.get('/', (req, res) => {
+  res.send('hello from express');
+});
+
+app.listen(PORT, () => `Listening on port ${PORT}`);
