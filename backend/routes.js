a6ceb0f19afce876bc75c03545f657b753845390
thapaRoyal <thaparoyal17@gmail.com>
2022-02-16 20:42:33 +0545

    setting router

--- backend/routes.js
+++ backend/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.post('/api/sendotp', (req, res) => {
   // logic
+  res.send('Hello from OTP route');
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
-
 const app = express();
+const router = require('./routes');
 
 const PORT = process.env.PORT || 5500;
 
 app.get('/', (req, res) => {
   res.send('hello from express');
 });
+
+app.use(router);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
