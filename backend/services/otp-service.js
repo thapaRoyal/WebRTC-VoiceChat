96ff58ab0b31d167c93ec933781ec024991f0b18
thapaRoyal <thaparoyal17@gmail.com>
2022-02-21 20:52:50 +0545

    setting up twilio

--- backend/services/otp-service.js
+++ backend/services/otp-service.js
@@ -1,12 +1,24 @@
 const crypto = require('crypto');
 
+const smsSid = process.env.SMS_SID;
+const smsAuthToken = process.env.SMS_AUTH_TOKEN;
+const twilio = require('twilio')(smsSid, smsAuthToken, {
+  lazyLoading: true,
+});
+
 class OtpService {
   async generateOtp() {
     const otp = crypto.randomInt(1000, 9999);
     return otp;
   }
 
-  sendBySms() {}
+  async sendBySms(phone, otp) {
+    return await twilio.messages.create({
+      to: phone,
+      from: process.env.SMS_FROM_NUMBER,
+      body: `Your Codershouse OTP is ${otp}`,
+    });
+  }
 
   verifyOtp() {}
 }
