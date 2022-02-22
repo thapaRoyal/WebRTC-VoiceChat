0c0fee06c7492f10ce40fd29b590db92a19a50c2
thapaRoyal <thaparoyal17@gmail.com>
2022-02-22 21:11:49 +0545

    tokens

--- backend/services/token-service.js
+++ backend/services/token-service.js
@@ -1,5 +1,18 @@
+const jwt = require('jsonwebtoken');
+const accessTokenSecret = process.env.JWT_ACCESS_TOKEN_SECRET;
+const refreshTokenSecret = process.env.JWT_REFRESH_TOKEN_SECRET;
 class TokenService {
-  generateTokens() {}
+  generateTokens(payload) {
+    const accessToken = jwt.sign(payload, accessTokenSecret, {
+      expiresIn: '1h',
+    });
+
+    const refreshToken = jwt.sign(payload, refreshTokenSecret, {
+      expiresIn: '1y',
+    });
+
+    return { accessToken, refreshToken };
+  }
 }
 
 module.exports = new TokenService();
