8e5f557ce29349ecc0bf07201e3e8babb83061b7
thapaRoyal <thaparoyal17@gmail.com>
2022-02-22 16:44:14 +0545

    DB Connected

--- backend/database.js
+++ backend/database.js
@@ -1,17 +1,17 @@
+// import statements
 const mongoose = require('mongoose');
-
 function DbConnect() {
   const DB_URL = process.env.DB_URL;
-  //   Database connection
+  // Database connection
   mongoose.connect(DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
   });
   const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'Connection error:'));
+  db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', () => {
-    console.log('DB Connected...');
+    console.log('DB connected...');
   });
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const DbConnect = require('./database');
 const router = require('./routes');
 
 const PORT = process.env.PORT || 5500;
+DbConnect();
 app.use(express.json());
 
 app.get('/', (req, res) => {
