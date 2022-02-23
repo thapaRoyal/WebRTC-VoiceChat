e31452bf3bc67a737f6100370525822fb1313ac0
thapaRoyal <thaparoyal17@gmail.com>
2022-02-23 20:23:31 +0545

    cors

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const DbConnect = require('./database');
 const router = require('./routes');
+const cors = require('cors');
+
+const corsOption = {
+  origin: ['http://localhost:3000'],
+};
+app.use(cors(corsOption));
 
 const PORT = process.env.PORT || 5500;
 DbConnect();
