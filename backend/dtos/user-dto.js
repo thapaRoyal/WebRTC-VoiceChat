d8c6a85432dc4ba61483b7667e3b4e8054dc3ed7
thapaRoyal <thaparoyal17@gmail.com>
2022-02-24 19:59:36 +0545

    user dto

---
+++ backend/dtos/user-dto.js
@@ -0,0 +1,15 @@
+class UserDto {
+  _id;
+  phone;
+  activated;
+  createdAt;
+
+  constructor(user) {
+    this._id = user._id;
+    this.phone = user.phone;
+    this.activated = user.activated;
+    this.createdAt = user.createdAt;
+  }
+}
+
+module.exports = UserDto;
