39da173b8cfde9b6f6d26ab8b90b5bedbe94db83
thapaRoyal <thaparoyal17@gmail.com>
2022-02-24 20:02:53 +0545

    using userDto

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -2,6 +2,7 @@ const otpService = require('../services/otp-service');
 const hashService = require('../services/hash-service');
 const userService = require('../services/user-service');
 const tokenService = require('../services/token-service');
+const UserDto = require('../dtos/user-dto');
 class AuthController {
   async sendOtp(req, res) {
     //Logic
@@ -75,7 +76,8 @@ class AuthController {
       httpOnly: true,
     });
 
-    res.json({ accessToken, user });
+    const userDto = new UserDto(user);
+    res.json({ accessToken, user: userDto });
   }
 }
 
--- backend/dtos/user-dto.js
+++ backend/dtos/user-dto.js
@@ -1,11 +1,11 @@
 class UserDto {
-  _id;
+  id;
   phone;
   activated;
   createdAt;
 
   constructor(user) {
-    this._id = user._id;
+    this.id = user._id;
     this.phone = user.phone;
     this.activated = user.activated;
     this.createdAt = user.createdAt;
