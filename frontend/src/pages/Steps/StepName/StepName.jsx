2c45205eb57f5cba2ea48843a3bc739ff9614c84
thapaRoyal <thaparoyal17@gmail.com>
2022-02-27 20:34:43 +0545

    activate sllice

--- frontend/src/pages/Steps/StepName/StepName.jsx
+++ frontend/src/pages/Steps/StepName/StepName.jsx
@@ -1,13 +1,29 @@
 // Import
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../../../components/shared/Button/Button';
+import Card from '../../../components/shared/Card/Card';
+import TextInput from '../../../components/shared/TextInput/TextInput';
 import styles from './StepName.module.css';
 
 // StepName Component
 const StepName = ({ onNext }) => {
+  const [name, setName] = useState('');
+  function nextStep() {
+    if (!name) {
+      return;
+    }
+  }
   return (
     <>
-      <div>StepName</div>
-      <button onClick={onNext}>Next</button>
+      <Card title="What's your full name ?" icon="smile">
+        <TextInput value={name} onChange={(e) => setName(e.target.value)} />
+        <p className={styles.bottomParagraph}>
+          People use real names at codershouse :)
+        </p>
+        <div className={styles.actionButtonWrap}>
+          <Button onClick={nextStep} text="Next" icon="arrow_forward" />
+        </div>
+      </Card>
     </>
   );
 };
