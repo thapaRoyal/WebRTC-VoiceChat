60bd4022c34be26f13925bbb600863b1fce6f109
thapaRoyal <thaparoyal17@gmail.com>
2022-03-02 21:49:53 +0545

    name dynamic

--- frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
+++ frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
@@ -3,16 +3,18 @@ import React from 'react';
 import styles from './StepAvatar.module.css';
 import Card from '../../../components/shared/Card/Card';
 import Button from '../../../components/shared/Button/Button';
+import { useSelector } from 'react-redux';
 
 // StepAvatar component
 const StepAvatar = ({ onNext }) => {
+  const { name } = useSelector((state) => state.activate);
   function submit() {
     //
   }
 
   return (
     <>
-      <Card title="Okay, Logan T!" icon="monkey">
+      <Card title={`Okay, ${name}!`} icon="monkey">
         <p className={styles.subHeading}>How,s this photo?</p>
         <div>
           <Button onClick={submit} text="Next" icon="arrow_forward" />
