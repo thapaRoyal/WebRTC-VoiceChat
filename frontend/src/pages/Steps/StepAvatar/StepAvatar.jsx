a38dd555029dcd4665aae5f8efe9201fd1bd93bc
thapaRoyal <thaparoyal17@gmail.com>
2022-03-04 20:10:15 +0545

    captureImage

--- frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
+++ frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
@@ -3,12 +3,25 @@ import React, { useState } from 'react';
 import styles from './StepAvatar.module.css';
 import Card from '../../../components/shared/Card/Card';
 import Button from '../../../components/shared/Button/Button';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { setAvatar } from '../../../store/activateSlice';
 
 // StepAvatar component
 const StepAvatar = ({ onNext }) => {
+  const dispatch = useDispatch();
   const { name } = useSelector((state) => state.activate);
   const [image, setImage] = useState('/images/monkey-avatar.png');
+  function captureImage(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      console.log(reader.result);
+      setImage(reader.result);
+      dispatch(setAvatar(reader.result));
+    };
+    console.log(e);
+  }
   function submit() {
     //
   }
@@ -21,7 +34,12 @@ const StepAvatar = ({ onNext }) => {
           <img className={styles.avatarImage} src={image} alt="Avatar" />
         </div>
         <div>
-          <input id="avatarInput" type="file" className={styles.avatarInput} />
+          <input
+            onChange={captureImage}
+            id="avatarInput"
+            type="file"
+            className={styles.avatarInput}
+          />
           <label className={styles.avatarLabel} htmlFor="avatarInput">
             Choose a different photo
           </label>
