de634b6e92c670524ab4c7a8e63331ee63556432
thapaRoyal <thaparoyal17@gmail.com>
2022-03-05 13:55:20 +0545

    activate route

--- backend/routes.js
+++ backend/routes.js
@@ -3,5 +3,6 @@ const authController = require('./controllers/auth-controller');
 
 router.post('/api/send-otp', authController.sendOtp);
 router.post('/api/verify-otp', authController.verifyOtp);
+router.post('/api/activate', activateController.activate);
 
 module.exports = router;
