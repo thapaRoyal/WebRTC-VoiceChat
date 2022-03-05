53165628d38a1c58427f60cf957b42169db05c8a
thapaRoyal <thaparoyal17@gmail.com>
2022-03-05 14:07:35 +0545

    creating middlewares

---
+++ backend/middlewares/auth-middleware.js
@@ -0,0 +1,9 @@
+module.exports = async function (req, res, next) {
+  try {
+  } catch (err) {
+    console.log(err);
+    s;
+  }
+
+  next();
+};
--- backend/routes.js
+++ backend/routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const authController = require('./controllers/auth-controller');
+const activateController = require('./controllers/activate-controller');
 
 router.post('/api/send-otp', authController.sendOtp);
 router.post('/api/verify-otp', authController.verifyOtp);
