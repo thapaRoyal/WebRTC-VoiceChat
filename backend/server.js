b77f007f3a50d0126f92e4dee84a0c87865fe982
thapaRoyal <thaparoyal17@gmail.com>
2022-03-05 14:51:20 +0545

    storage

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const corsOption = {
   origin: ['http://localhost:3000'],
 };
 app.use(cors(corsOption));
+app.use('/storage', express.static('storage'));
 
 const PORT = process.env.PORT || 5500;
 DbConnect();
