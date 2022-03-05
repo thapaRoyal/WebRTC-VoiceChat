fd3ba94b0f67e92f35530e5ea658a5d1a5b4c71a
thapaRoyal <thaparoyal17@gmail.com>
2022-03-05 14:25:32 +0545

    base64

--- backend/controllers/activate-controller.js
+++ backend/controllers/activate-controller.js
@@ -1,6 +1,16 @@
 class ActivateController {
   async activate(req, res) {
     // Activate logic
+    const { name, avatar } = req.body;
+    if (!name || !avatar) {
+      res.status(400).json({ message: 'All fields are required' });
+    }
+
+    // Image Base64
+    const buffer = Buffer.from(
+      avatar.replace(/^data:image\/\w+;base64,/, ''),
+      'base64'
+    );
     res.json({ message: 'OK!' });
   }
 }
