c03a422999da32ce5208823236601c952b63b162
thapaRoyal <thaparoyal17@gmail.com>
2022-03-05 14:13:08 +0545

    apply middleware || get access token

--- backend/middlewares/auth-middleware.js
+++ backend/middlewares/auth-middleware.js
@@ -1,6 +1,7 @@
 module.exports = async function (req, res, next) {
   try {
     const { accessToken } = req.cookies;
+    console.log(accessToken);
   } catch (err) {
     console.log(err);
     s;
