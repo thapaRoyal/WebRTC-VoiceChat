23606afbffb2e6cc70390b2daabdad6089dc315a
thapaRoyal <thaparoyal17@gmail.com>
2022-03-07 20:43:07 +0545

    refresh route

--- backend/routes.js
+++ backend/routes.js
@@ -6,5 +6,6 @@ const authMiddleware = require('./middlewares/auth-middleware');
 router.post('/api/send-otp', authController.sendOtp);
 router.post('/api/verify-otp', authController.verifyOtp);
 router.post('/api/activate', authMiddleware, activateController.activate);
+router.post('/api/refresh', authController.refresh);
 
 module.exports = router;
