eaab116f97e73551f9a5ca0bd52e31e9638a5d7e
thapaRoyal <thaparoyal17@gmail.com>
2022-03-12 13:21:58 +0545

    rooms left

--- frontend/src/pages/Rooms/Rooms.jsx
+++ frontend/src/pages/Rooms/Rooms.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styles from './Rooms.module.css';
 
 const Rooms = () => {
-  return <div>Rooms Component</div>;
+  return (
+    <>
+      <div className="container">
+        <div className={styles.roomsHeader}>
+          <div className={styles.left}>
+            <span className={styles.heading}>All voice rooms</span>
+            <div className={styles.searchBox}>
+              <img src="/images/search-icon.png" alt="Search" />
+              <input type="text" className={styles.searchInput} />
+            </div>
+          </div>
+          <div className={styles.right}></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Rooms;
