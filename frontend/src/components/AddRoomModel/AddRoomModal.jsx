111f8bd50ff6d884f27cc82878f49fa7cf60c18e
thapaRoyal <thaparoyal17@gmail.com>
2022-03-15 20:38:29 +0545

    add room modal  ksx

--- frontend/src/components/AddRoomModel/AddRoomModal.jsx
+++ frontend/src/components/AddRoomModel/AddRoomModal.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import styles from './AddRoomModal.module.css';
+import TextInput from '../shared/TextInput/TextInput';
 
 const AddRoomModel = () => {
-  return <div>AddRoomModel</div>;
+  return (
+    <div className={styles.modalMask}>
+      <div className={styles.modalBody}>
+        <div className={styles.modalHeader}>
+          <h3>Enter the topic to be discussed</h3>
+          <TextInput />
+        </div>
+        <div className={styles.modalFooter}></div>
+      </div>
+    </div>
+  );
 };
 
 export default AddRoomModel;
