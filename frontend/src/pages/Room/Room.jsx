8c3bb29c81132554fc5f01291044a20d0012ecc8
thapaRoyal <thaparoyal17@gmail.com>
2022-03-17 16:38:58 +0545

    room audio element

--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Room.module.css';
 
 const Room = () => {
-  return <div>Single Room</div>;
+  const [clients, setClient] = useState([
+    {
+      id: 1,
+      name: 'Royal T',
+    },
+    {
+      id: 2,
+      name: 'Thapa R',
+    },
+  ]);
+  return (
+    <div>
+      <h1>All connected rooms</h1>
+      {clients.map((client) => {
+        return (
+          <div>
+            <audio controls autoPlay></audio>
+            <h4>{client.name}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Room;
