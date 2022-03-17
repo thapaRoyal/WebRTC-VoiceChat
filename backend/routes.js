0c38d2cf9c150699676e3cfae9358305741f0c3b
thapaRoyal <thaparoyal17@gmail.com>
2022-03-17 12:22:45 +0545

    routes

--- backend/routes.js
+++ backend/routes.js
@@ -8,5 +8,6 @@ router.post('/api/verify-otp', authController.verifyOtp);
 router.post('/api/activate', authMiddleware, activateController.activate);
 router.get('/api/refresh', authController.refresh);
 router.post('/api/logout', authMiddleware, authController.logout);
+router.post('/api/rooms', authMiddleware, roomsController.create);
 
 module.exports = router;
