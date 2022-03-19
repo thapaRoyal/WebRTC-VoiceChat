34a0f0c35c60eccecdd68118bac3dfe4be7e2932
thapaRoyal <thaparoyal17@gmail.com>
2022-03-19 19:02:48 +0545

    back

--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useWebRTC } from '../../hooks/useWebRTC';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import styles from './Room.module.css';
 
@@ -8,9 +8,20 @@ const Room = () => {
   const { id: roomId } = useParams();
   const user = useSelector((state) => state.auth.user);
   const { clients, provideRef } = useWebRTC(roomId, user);
+  const history = useHistory();
+
+  const handleManualLeave = () => {
+    history.push('/rooms');
+  };
 
   return (
     <div>
+      <div className="container">
+        <button onClick={handleManualLeave} className={styles.goBack}>
+          <img src="/images/arrow-left.png" alt="arrow left" />
+          <span>All voice rooms</span>
+        </button>
+      </div>
       <h1>All connected rooms</h1>
       {clients.map((client) => {
         return (
