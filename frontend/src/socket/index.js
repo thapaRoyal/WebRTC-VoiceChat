e59a07313a8994666b13c76194648c3191e42454
thapaRoyal <thaparoyal17@gmail.com>
2022-03-19 11:13:07 +0545

    socketInit

--- frontend/src/socket/index.js
+++ frontend/src/socket/index.js
@@ -1,1 +1,11 @@
 import { io } from 'socket.io-client';
+
+const socketInit = () => {
+  const options = {
+    'force new connection': true,
+    reconnectionAttempt: 'Infinity',
+    timeout: 10000,
+    transports: ['websocket'],
+  };
+  return io('http://localhost:5500', options);
+};
