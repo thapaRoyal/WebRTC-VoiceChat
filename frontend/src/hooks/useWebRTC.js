3be9630acb2f6f11b7a89518a85510ce16f045b8
thapaRoyal <thaparoyal17@gmail.com>
2022-03-19 10:54:03 +0545

    remov dummy data

--- frontend/src/hooks/useWebRTC.js
+++ frontend/src/hooks/useWebRTC.js
@@ -1,19 +1,8 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { useStateWithCallback } from './useStateWithCallback';
 
-const users = [
-  {
-    id: 1,
-    name: 'Royal T',
-  },
-  {
-    id: 2,
-    name: 'Thapa R',
-  },
-];
-
 export const useWebRTC = (roomId, user) => {
-  const [clients, setClients] = useStateWithCallback(users);
+  const [clients, setClients] = useStateWithCallback([]);
 
   const audioElements = useRef({});
   const connections = useRef({});
